a4262a321ad92549b1f831c3512eef769f67550c
liujohnson118 <liujohnson118@yahoo.com>
2017-07-16 00:06:23 +0000

    automatically selecting autopilot when self drive selected

---
+++ app/assets/javascripts/ap_sd_select.js
@@ -0,0 +1,11 @@
+var sd = document.getElementById('car_sd');
+if(sd){
+  sd.addEventListener('change', function(){
+    if( sd.checked) {
+      console.log("sd is checked");
+      document.getElementById('car_ap').checked=true;
+    } else {
+      //chk2.attr('checked', false);
+    }
+  });
+}
--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,4 +1,7 @@
 class CarsController < ApplicationController
+
+  # GET '/model3/new'
+  # User must log in to customize order, otherwise redirect to login
   def new
     if session[:user_id] == nil
       redirect_to '/login'
@@ -6,6 +9,8 @@ class CarsController < ApplicationController
     end
   end
 
+  # POST '/model3/new'
+  # Upon successful validation of parameters, create car to Car db and redirect to /mycars
   def create
     my_params=car_params
     my_params[:user_id]=session[:user_id]
@@ -14,6 +19,8 @@ class CarsController < ApplicationController
     redirect_to '/mycars'
   end
 
+  # GET '/cars/index'
+  # If logged in, show all the cars customized by the user, otherwise redirect to login
   def index
     if session[:user_id] == nil
       redirect_to '/login'
@@ -22,13 +29,20 @@ class CarsController < ApplicationController
     end
   end
 
+  # DELETE 'cars/:id'
+  # Delete car by id as in params and redirect to /mycars
   def destroy
     @car_to_delete=Car.find(params[:id])
     @car_to_delete.destroy
     redirect_to '/mycars'
   end
 
+   #Private methods
    private
+    # Method to get car parameters for creation of a car and store in Car db
+    # sd: self-driving
+    # ap: autopilot
+    # awd: all wheel drive
     def car_params
       params.require(:car).permit(:sd, :ap, :awd, :color, :battery,:wheel)
     end
