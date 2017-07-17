0189aa8437d1171f38a78f044d68c3dc75004e0f
liujohnson118 <liujohnson118@yahoo.com>
2017-07-17 20:15:02 +0000

    consumption seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,22 @@ prices=Price.create(
     wheel_18_price: 500, wheel_19_price: 750, base_price: 35000}
   ])
 
+cars=Car.create(
+  [{color: "red", ap: true, awd: false, sd: true, battery: 80, wheel: 17, user_id: 1, paid: false, price: 59000.0, reviewed: false},
+    {color: "white", ap: false, awd: false, sd: false, battery: 70, wheel: 17, user_id: 1, paid: false, price: 42500.0, reviewed: false},
+    {color: "white", ap: false, awd: false, sd: false, battery: 60, wheel: 17, user_id: 3, paid: false, price: 35000.0, reviewed: false},
+    {color: "red", ap: true, awd: false, sd: false, battery: 80, wheel: 17, user_id: 1, paid: false, price: 51200.0, reviewed: false},
+    {color: "red", ap: true, awd: false, sd: true, battery: 80, wheel: 17, user_id: 1, paid: false, price: 59000.0, reviewed: false},
+    {color: "white", ap: false, awd: false, sd: false, battery: 70, wheel: 17, user_id: 2, paid: false, price: 42500.0, reviewed: false},
+    {color: "white", ap: false, awd: true, sd: false, battery: 70, wheel: 17, user_id: 2, paid: false, price: 47500.0, reviewed: false},
+    {color: "red", ap: true, awd: false, sd: false, battery: 60, wheel: 17, user_id: 2, paid: false, price: 39200.0, reviewed: false},
+    {color: "red", ap: true, awd: false, sd: true, battery: 80, wheel: 17, user_id: 3, paid: false, price: 59000.0, reviewed: false},
+    {color: "white", ap: false, awd: false, sd: false, battery: 70, wheel: 17, user_id: 3, paid: false, price: 42500.0, reviewed: false},
+    {color: "white", ap: false, awd: true, sd: false, battery: 70, wheel: 17, user_id: 3, paid: false, price: 47500.0, reviewed: false},
+    {color: "red", ap: true, awd: false, sd: false, battery: 60, wheel: 17, user_id: 2, paid: false, price: 39200.0, reviewed: false},
+  ])
+
+consumptions=Consumption.create(
+  [{car_id: 1, battery: 80, range:500},{car_id: 1, battery: 80, range:510},{car_id: 1, battery: 80, range:520},{car_id: 1, battery: 80, range:470},{car_id: 1, battery: 80, range:440},
+    {car_id: 2, battery: 70, range:400},{car_id: 2, battery: 70, range:410},{car_id: 2, battery: 70, range:405},{car_id: 2, battery: 70, range:390},{car_id: 2, battery: 70, range:388},
+    {car_id: 3, battery: 60, range:300},{car_id: 3, battery: 60, range:340},{car_id: 3, battery: 60, range:330},{car_id: 3, battery: 60, range:280},{car_id: 3, battery: 60, range:290}])
