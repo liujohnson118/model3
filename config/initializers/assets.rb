7be88eb562269adb5143690cc08aebfc784f3595
liujohnson118 <liujohnson118@yahoo.com>
2017-07-17 20:41:51 +0000

    atual range in google map implemented

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( map.js )
+Rails.application.config.assets.precompile += %w( actual_range.js )
 Rails.application.config.assets.precompile += %w( ap_sd_select.js )
 
 Rails.application.config.assets.precompile += %w( homepage.css )
